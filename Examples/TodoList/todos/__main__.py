5d29aab61d7a3b3ffc788590e49f732960b63464
Matthias Hölzl <tc@xantira.com>
2021-12-13 12:12:42 +0100

    Update Todo-list example

--- Examples/TodoList/todos/__main__.py
+++ Examples/TodoList/todos/__main__.py
@@ -1,49 +1,78 @@
 import argparse
-
+import pickle
+from pathlib import Path
+import typer
 from .todo_list import TodoList
 
-
-def get_args():
-    parser = argparse.ArgumentParser('Manage a TODO list.')
-
-    # Cannot use a file type, since the required mode will only be known later
-    parser.add_argument('file')
-    opts = parser.add_mutually_exclusive_group(required=True)
-    opts.add_argument('--create', action='store_true')
-    opts.add_argument('--add', nargs=2, metavar=('title', 'priority'))
-    opts.add_argument('--delete', metavar='title')
-    opts.add_argument('--mark-completed', metavar='title')
-    opts.add_argument('--delete-all-completed', action='store_true')
-    opts.add_argument('--list', action='store_true')
-
-    return parser.parse_args()
+app = typer.Typer()
 
 
-def main():
-    args = get_args()
-    if args.create:
-        with open(args.file, 'wb'):
-            pass
-    else:
-        save_todos = True
-        with open(args.file, 'rb') as file:
-            todos = TodoList.load_from_file(file)
-        if args.add:
-            title, priority = args.add
-            todos.add(title, int(priority))
-        elif args.delete is not None:
-            todos.delete(args.delete)
-        elif args.mark_completed is not None:
-            todos.mark_completed(args.mark_completed)
-        elif args.delete_all_completed:
-            todos.delete_all_completed()
-        else:
-            save_todos = False
-            print(todos)
-        if save_todos:
-            with open(args.file, 'wb') as file:
-                todos.save_to_file(file)
+@app.command()
+def create(file: Path = typer.Argument(..., help="the file to store the todo list in")):
+    """
+    Create a new todo list.
+    """
+    with file.open("wb") as f:
+        pickle.dump(TodoList([]), f)
 
 
-if __name__ == '__main__':
-    main()
+@app.command()
+def add(
+    file: Path = typer.Argument(..., help="the todo list"),
+    title: str = typer.Argument(..., help="the title of the todo item"),
+    priority: int = typer.Option(1, help="the priority of the item"),
+):
+    """
+    Add a new item.
+    """
+    with file.open("rb") as f:
+        todos = TodoList.load_from_file(f)
+    todos.add(title, priority)
+    with file.open("wb") as f:
+        todos.save_to_file(f)
+
+
+@app.command()
+def delete(
+    file: Path = typer.Argument(..., help="the todo list"),
+    title: str = typer.Argument(..., help="the title of the item to delete"),
+):
+    """
+    Delete an item.
+    """
+    with file.open("rb") as f:
+        todos = TodoList.load_from_file(f)
+    todos.delete(title)
+    with file.open("wb") as f:
+        todos.save_to_file(f)
+
+
+@app.command()
+def mark_completed(
+    file: Path = typer.Argument(..., help="the todo list"),
+    title: str = typer.Argument(..., help="the title of the item"),
+):
+    """
+    Mark an item as completed.
+    """
+    with file.open("rb") as f:
+        todos = TodoList.load_from_file(f)
+    todos.mark_completed(title)
+    with file.open("wb") as f:
+        todos.save_to_file(f)
+
+
+@app.command()
+def list(
+    file: Path = typer.Argument(..., help="the todo list"),
+):
+    """
+    Show all items in the list.
+    """
+    with file.open("rb") as f:
+        todos = TodoList.load_from_file(f)
+    print(todos)
+
+
+if __name__ == "__main__":
+    app()
