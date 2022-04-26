5f4686250fd90473d4779ba736d4c44c3e53eb27
Matthias Hölzl <tc@xantira.com>
2022-04-26 13:22:00 +0200

    Minor updates to examples

--- Examples/ModuleTest/run.py
+++ Examples/ModuleTest/run.py
@@ -1,12 +1,12 @@
 import printer
 import names
 
-print("Loading foo.py")
+print("Loading run.py")
 print(f"__name__ is {__name__}")
 
 
 def run_program():
-    print("foo.py started as main program.")
+    print("run.py started as main program.")
     printer.print_greeting(names.get_name())
     print(printer.my_fun())
     print(names.my_fun())
