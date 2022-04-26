1d99cad4844e7c3b353aeb0b75badda91b49ae3f
Matthias Hölzl <tc@xantira.com>
2022-04-26 02:33:17 +0200

    End of day 1

--- Examples/SimpleExamples/no-output.py
+++ Examples/SimpleExamples/no-output.py
@@ -1,1 +1,27 @@
+# %%
 123
+
+# %%
+print("Hello, world!")
+
+# %%
+234
+
+
+# %%
+def bad_fun(x: int, y, z):
+    print(
+        "This is a very, very, very, very, very, very, very, very, "
+        "very, very, very, very, very, very, very, very, very, very, "
+        "very, very, very, very, very, very, very, very, very, "
+        "very, very long string"
+    )
+
+
+# %%
+def mult(a: int, b: float) -> float:
+    return a * b
+
+
+# %%
+mult("foo", 2)
